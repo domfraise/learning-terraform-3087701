e2dfe79fb239e1cf48dd26dc2587cfc80f5e01a1
domfraise <domfraise@gmail.com>
2025-10-22 11:41:10 +0100

    fix

--- main.tf
+++ main.tf
@@ -72,7 +72,7 @@ module "blog_alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      target_id        = module.autoscaling.id
+      target_id        = module.autoscaling.target_groups
     }
   }
 
